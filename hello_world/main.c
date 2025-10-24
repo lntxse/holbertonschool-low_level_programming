6eedfdb8646183bc84eb5ee076d3b776159111ea
lntxse <hent4aianime@gmail.com>
2025-10-24 11:53:59 +0000

    m

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,1 +1,14 @@
-ntm
+   # include <stdio.h> 
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints "Hello, World!" to the standard output.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    puts("Hello, World!");
+    return (0);
+}
