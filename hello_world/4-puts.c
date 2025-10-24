0e1ceb61777d667d0aa6b3229bfc2cd8d9113fbe
lntxse <hent4aianime@gmail.com>
2025-10-24 08:16:08 +0000

    m4444

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - point d'entrée du programme
+*
+* Return: toujours 0 (succès)
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
